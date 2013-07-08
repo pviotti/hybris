70f12412a64b3682791086d212c057d8870b35fb
pviotti <paolo@galahad>
2013-07-08 11:36:08 +0200

    completed test cli tool

--- src/test/java/fr/eurecom/hybris/test/HybrisTestCli.java
+++ src/test/java/fr/eurecom/hybris/test/HybrisTestCli.java
@@ -3,17 +3,24 @@ package fr.eurecom.hybris.test;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Map;
 
 import fr.eurecom.hybris.Hybris;
 import fr.eurecom.hybris.HybrisException;
+import fr.eurecom.hybris.mds.Metadata;
 
 public class HybrisTestCli implements Runnable {
     
+    private Hybris hybris;
     private static String HELP_STRING = "Usage:\n" +
                                         "\th - help\n" +
-                                        "\tq - quit";
-    
-    private Hybris hybris;
+                                        "\tq - quit\n" +
+                                        "\tw [key] [value] - write\n" +
+                                        "\tr [key] - read\n" +
+                                        "\td [key] - delete\n" +
+                                        "\tl - list\n" +
+                                        "\tla - list all";
     
     public HybrisTestCli() throws HybrisException {
         hybris = new Hybris();
@@ -33,14 +40,31 @@ public class HybrisTestCli implements Runnable {
                 System.out.print(">");
                 line = br.readLine().trim().toLowerCase();
                 
-                if (line.equalsIgnoreCase("q"))
+                if (line.equals("q"))
                     quit = true;
-                else if (line.equalsIgnoreCase("h"))
+                else if (line.equals("h"))
                     System.out.println(HELP_STRING);
-                else
+                else if (line.startsWith("w")) {
+                    String key = line.split(" ")[1];
+                    byte[] value = line.split(" ")[2].getBytes();
+                    this.write(key, value);
+                } else if (line.startsWith("r")) {
+                    String key = line.split(" ")[1];
+                    byte[] value = this.read(key);
+                    System.out.println("Value retrieved: " + new String(value));
+                } else if (line.startsWith("d")) {
+                    String key = line.split(" ")[1];
+                    this.delete(key);
+                } else if (line.equals("l")) {
+                    List<String> list = this.list();
+                    for (String key : list)
+                        System.out.println("\t - " + key);
+                } else if (line.equals("la")) {
+                    Map<String, Metadata> map = this.getAllMetadata();
+                    for(String key : map.keySet()) 
+                        System.out.println("\t - " + key + ": " + map.get(key));
+                } else
                     System.out.println("* Unknown command.");
-                
-                // TODO
             }
         } catch (IOException ioe) {
            ioe.printStackTrace();
@@ -48,6 +72,48 @@ public class HybrisTestCli implements Runnable {
         }
     }
     
+    private void write(String key, byte[] value) {
+        try {
+            hybris.write(key, value);
+        } catch (HybrisException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private byte[] read(String key) {
+        try {
+            return hybris.read(key);
+        } catch (HybrisException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    private void delete(String key) {
+        try {
+            hybris.delete(key);
+        } catch (HybrisException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private List<String> list() {
+        try {
+            return hybris.list();
+        } catch (HybrisException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    private Map<String, Metadata> getAllMetadata() {
+        try {
+            return hybris.getAllMetadata();
+        } catch (HybrisException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
     public static void main(String[] args) throws HybrisException {
         HybrisTestCli htc = new HybrisTestCli();
